f8e3cb0a7e3e036d894c93aaa2c8178ad6101082
vyuldashev <misterio92@gmail.com>
2018-03-07 00:17:49 +0300

    tests

--- tests/Unit/TelegramHandlerTest.php
+++ tests/Unit/TelegramHandlerTest.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace VladimirYuldashev\Monolog\Tests\Unit;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Monolog\Logger;
 use PHPUnit\Framework\TestCase;
@@ -13,10 +15,26 @@ use VladimirYuldashev\Monolog\TelegramHandler;
 
 class TelegramHandlerTest extends TestCase
 {
-    public function test()
+    public function testSuccess(): void
     {
         $mock = new MockHandler([
             new Response()
+        ]);
+        $client = new Client(['handler' => $mock]);
+
+        $handler = new TelegramHandler(Logger::DEBUG, 'foo', 1, true, $client);
+
+        $logger = new Logger('handler', [$handler]);
+
+        $result = $logger->debug('test', ['foo' => 'bar']);
+
+        $this->assertTrue($result);
+    }
+
+    public function testCatchesException(): void
+    {
+        $mock = new MockHandler([
+            new RequestException('Error Communicating with Server', new Request('GET', 'telegram'))
         ]);
         $client = new Client(['handler' => $mock]);
 
